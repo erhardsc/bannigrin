2e6054e37f6be3f7f1026df87847b9303905529c
Sumner <serhard2556@gmail.com>
2018-01-03 16:39:02 -0700

    Removed old themes and added initialized SCSS for all style edits

--- themes/themify-shoppe-child/functions.php
+++ themes/themify-shoppe-child/functions.php
@@ -8,14 +8,10 @@ function themify_custom_enqueue_child_theme_styles() {
 add_action( 'wp_enqueue_scripts', 'themify_custom_enqueue_child_theme_styles', 11 );
 
 
-function grayson_custom_enqueue_child_theme_styles() {
-    wp_enqueue_style( 'grayson', get_stylesheet_directory_uri() . '/grayson/grayson.css' );
-}
-add_action( 'wp_enqueue_scripts', 'grayson_custom_enqueue_child_theme_styles', 20 );
+function enqueue_child_theme_styles_scripts() {
+  wp_enqueue_style( 'css', get_stylesheet_directory_uri() . '/css/style.css' );
 
-function custom_js() {
-	
-	wp_enqueue_script( 'app', get_stylesheet_directory_uri() . '/js/app.js', array('jquery'), true );
+  wp_enqueue_script( 'app', get_stylesheet_directory_uri() . '/js/app.js', array('jquery'), true );
 
 }
-add_action( 'wp_enqueue_scrips', custom_js() );
+add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles_scripts', 20 );
