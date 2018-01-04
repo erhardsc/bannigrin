78c517b58730ebe7be6586e2fb070dc9b20d57e5
Sumner <serhard2556@gmail.com>
2018-01-04 02:16:04 -0700

    Added Lessons div to homepage. Polished up more styles

--- themes/themify-shoppe-child/functions.php
+++ themes/themify-shoppe-child/functions.php
@@ -9,6 +9,8 @@ add_action( 'wp_enqueue_scripts', 'themify_custom_enqueue_child_theme_styles', 1
 
 
 function enqueue_child_theme_styles_scripts() {
+
+  // To edit styles, go to the sass folder
   wp_enqueue_style( 'css', get_stylesheet_directory_uri() . '/css/style.css' );
 
   wp_enqueue_script( 'app', get_stylesheet_directory_uri() . '/js/app.js', array('jquery'), true );
