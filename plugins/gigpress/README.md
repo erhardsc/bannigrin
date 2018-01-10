79a12af34b18fd17239d5ecc88e2feed0191be65
Sumner <serhard2556@gmail.com>
2018-01-09 22:16:41 -0700

    Cleaning up

--- plugins/gigpress/README.md
+++
@@ -1,1 +0,0 @@
-[GigPress](http://gigpress.com) is a live performance listing and management plugin that's been serving musicians and performers since 2007.
---
+++ uploads/wp-migrate-db/bannigrin-migrate-20180104215216-meyo4.sql
@@ -0,0 +1,99 @@
+# WordPress MySQL database migration
+#
+# Generated: Thursday 4. January 2018 21:52 UTC
+# Hostname: localhost
+# Database: `bannigrin`
+# URL: //bannigrin.com
+# Path: /home2/bannigrin/public_html
+# Tables: wp_commentmeta, wp_comments, wp_huge_it_videogallery_galleries, wp_huge_it_videogallery_videos, wp_links, wp_options, wp_postmeta, wp_posts, wp_term_relationships, wp_term_taxonomy, wp_termmeta, wp_terms, wp_usermeta, wp_users
+# Table Prefix: wp_
+# Post Types: revision, attachment, customize_changeset, nav_menu_item, page, post, tbuilder_layout
+# Protocol: http
+# Multisite: false
+# Subsite Export: false
+# --------------------------------------------------------
+
+/*!40101 SET NAMES utf8mb4 */;
+
+SET sql_mode='NO_AUTO_VALUE_ON_ZERO';
+
+
+
+#
+# Delete any existing table `wp_commentmeta`
+#
+
+DROP TABLE IF EXISTS `wp_commentmeta`;
+
+
+#
+# Table structure of table `wp_commentmeta`
+#
+
+CREATE TABLE `wp_commentmeta` (
+  `meta_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `comment_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `meta_key` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `meta_value` longtext COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`meta_id`),
+  KEY `comment_id` (`comment_id`),
+  KEY `meta_key` (`meta_key`(191))
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+#
+# Data contents of table `wp_commentmeta`
+#
+
+#
+# End of data contents of table `wp_commentmeta`
+# --------------------------------------------------------
+
+
+
+#
+# Delete any existing table `wp_comments`
+#
+
+DROP TABLE IF EXISTS `wp_comments`;
+
+
+#
+# Table structure of table `wp_comments`
+#
+
+CREATE TABLE `wp_comments` (
+  `comment_ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `comment_post_ID` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `comment_author` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  `comment_author_email` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `comment_author_url` varchar(200) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `comment_author_IP` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `comment_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `comment_date_gmt` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `comment_content` text COLLATE utf8_unicode_ci NOT NULL,
+  `comment_karma` int(11) NOT NULL DEFAULT '0',
+  `comment_approved` varchar(20) COLLATE utf8_unicode_ci NOT NULL DEFAULT '1',
+  `comment_agent` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `comment_type` varchar(20) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `comment_parent` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `user_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`comment_ID`),
+  KEY `comment_post_ID` (`comment_post_ID`),
+  KEY `comment_approved_date_gmt` (`comment_approved`,`comment_date_gmt`),
+  KEY `comment_date_gmt` (`comment_date_gmt`),
+  KEY `comment_parent` (`comment_parent`),
+  KEY `comment_author_email` (`comment_author_email`(10))
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+#
+# Data contents of table `wp_comments`
+#
+INSERT INTO `wp_comments` ( `comment_ID`, `comment_post_ID`, `comment_author`, `comment_author_email`, `comment_author_url`, `comment_author_IP`, `comment_date`, `comment_date_gmt`, `comment_content`, `comment_karma`, `comment_approved`, `comment_agent`, `comment_type`, `comment_parent`, `user_id`) VALUES
+(1, 1, 'A WordPress Commenter', 'wapuu@wordpress.example', 'https://wordpress.org/', '', '2017-07-01 20:42:34', '2017-07-01 20:42:34', 'Hi, this is a comment.\nTo get started with moderating, editing, and deleting comments, please visit the Comments screen in the dashboard.\nCommenter avatars come from <a href="https://gravatar.com">Gravatar</a>.', 0, '1', '', '', 0, 0) ;
+
+#
+# End of data contents of table `wp_comments`
+# --------------------------------------------------------
+
